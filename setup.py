83a717e054006a2809eff3cdbba249ea2a21d89e
Esgr0bar <163046224+Esgr0bar@users.noreply.github.com>
2024-08-27 14:40:38 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="cyber_forensics_toolkit",
+    name="cloud_forensics_toolkit",
     version="1.0.0",
     description="A Python toolkit for cyber forensics across multiple cloud platforms.",
     packages=find_packages(),
@@ -17,7 +17,7 @@ setup(
     ],
     entry_points={
         'console_scripts': [
-            'cyber_forensics_toolkit=main:main',
+            'cloud_forensics_toolkit=main:main',
         ],
     },
     python_requires='>=3.8',
