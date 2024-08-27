39fc57d604676f9438f0070653ea3d413c8060f0
Esgr0bar <163046224+Esgr0bar@users.noreply.github.com>
2024-08-27 14:23:26 +0200

    Update packet_capture.py

--- forensics/packet_capture.py
+++ forensics/packet_capture.py
@@ -1,11 +1,16 @@
+import subprocess
 from scapy.all import sniff, wrpcap
 
 class PacketCapture:
     def capture_packets(self, iface, output_file):
         packets = sniff(iface=iface, timeout=60)
         wrpcap(output_file, packets)
-        return output_file
+        return f"Packets captured and saved to {output_file}"
 
     def analyze_packets(self, pcap_file):
-        #to do  Use Scapy or Tshark for analysis
-        return "Packet Analysis Completed"
+        # Analyze packets using TShark
+        analysis_output = f"{pcap_file}_analysis.txt"
+        subprocess.run([
+            "tshark", "-r", pcap_file, "-q", "-z", f"io,stat,0", ">", analysis_output
+        ], shell=True)
+        return f"Packet analysis completed. Output saved to {analysis_output}"
